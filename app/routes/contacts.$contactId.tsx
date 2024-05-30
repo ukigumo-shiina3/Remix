f3b9139abdf8bcbfaa47023ea538c40c6ddd7466
ukigumo-shiina3 <daisukeyao@outlook.jp>
2024-05-31 00:00:48 +0900

    ⚡ params.contactIdがない場合の404エラー表示

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -3,9 +3,15 @@ import { Form, useLoaderData } from "@remix-run/react";
 import type { FunctionComponent } from "react";
 import type { ContactRecord } from "../data";
 import { getContact } from "../data";
+import invariant from "tiny-invariant";
+import type { LoaderFunctionArgs } from "@remix-run/node";
 
-export const loader = async ({ params }) => {
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
   return json({ contact });
 };
 
