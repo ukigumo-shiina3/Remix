e34b218e88ed8795e1c29e21da57d874c32bc94e
ukigumo-shiina3 <daisukeyao@outlook.jp>
2024-06-10 21:39:25 +0900

    ✨ リストのフィルター検索

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import { json, redirect } from "@remix-run/node";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import {
   Form,
   Link,
@@ -19,8 +19,10 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
 ];
 
-export const loader = async () => {
-  const contacts = await getContacts();
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
   return json({ contacts });
 };
 
