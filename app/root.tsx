ee0d92ff4bad6ae5f44daabe2d6183dc137eda23
ukigumo-shiina3 <daisukeyao@outlook.jp>
2024-06-11 22:57:00 +0900

    ⚡ 検索後のページ更新時、フォームフィールドの値を保持する

--- app/root.tsx
+++ app/root.tsx
@@ -23,7 +23,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return json({ contacts });
+  return json({ contacts, q });
 };
 
 export const action = async () => {
@@ -32,7 +32,7 @@ export const action = async () => {
 };
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
+  const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
 
   return (
@@ -51,6 +51,7 @@ export default function App() {
               <input
                 id="q"
                 aria-label="Search contacts"
+                defaultValue={q || ""}
                 placeholder="Search"
                 type="search"
                 name="q"
